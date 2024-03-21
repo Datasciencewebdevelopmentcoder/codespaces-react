9e88081ded9da2923a813cd4f546a8c1b5ede3f7
Datasciencewebdevelopmentcoder <154837114+Datasciencewebdevelopmentcoder@users.noreply.github.com>
2024-03-21 01:36:56 +0000

    added some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
+  const title = "Hello"
+  const like = 50
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <Navbar/>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {like} times</p>
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = function () {
+    return (
+        <nav className="navbar">
+        <h1>The Blog Dojo</h1>
+        <div className="links">
+        <a href="/">Home</a>
+        <a href="create"> New Blog</a>
+        </div>
+        </nav>
+    );
+}
+export default Navbar
