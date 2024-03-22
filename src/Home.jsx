9908c1a5217a32eaeb48fae7f8c24593c55482cc
Datasciencewebdevelopmentcoder <154837114+Datasciencewebdevelopmentcoder@users.noreply.github.com>
2024-03-22 00:49:47 +0000

    completed lesson 10, css, useState, map

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs , setBlogs] = useState([
+        {
+            id: 1,
+            title: "Introduction to JavaScript",
+            body: "JavaScript is a popular programming language used for web development.",
+            author: "John Doe"
+          },
+          {
+            id: 2,
+            title: "Advanced JavaScript Techniques",
+            body: "Learn about closures, promises, and other advanced JavaScript concepts.",
+            author: "Jane Smith"
+          },
+          {
+            id: 3,
+            title: "JavaScript Frameworks Comparison",
+            body: "Compare popular JavaScript frameworks like React, Angular, and Vue.",
+            author: "Michael Johnson"
+          }
+    ])
+    const fun = (name) => {
+        console.log(name)
+    }
     return ( <div className="home">
-        <h2>home page</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>{blog.body}</p>
+                <p> written by {blog.author}</p>
+                
+                </div>
+            ))}
     </div> );
 }
  
